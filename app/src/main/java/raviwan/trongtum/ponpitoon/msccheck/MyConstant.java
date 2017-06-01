1c21f1d9598cc73b5b4675badb23f9a7c5b540a1
User <ponpioon.dome@gmail.com>
2017-06-01 16:04:34 +0700

    finish1june

---
+++ app/src/main/java/raviwan/trongtum/ponpitoon/msccheck/MyConstant.java
@@ -0,0 +1,31 @@
+package raviwan.trongtum.ponpitoon.msccheck;
+
+/**
+ * Created by User on 1/6/2560.
+ */
+
+public class MyConstant {
+
+    //For Spinner
+    private String[] titleStrings = new String[]{"Mr","Miss","Mrs"};
+    private String[] yearStrings = new String[]{"1","2","3","4"};
+    private String[] majorStrings = new String[]{"Major A","Major B","Major C","Major D"};
+    private String[] classStrings = new String[]{"Class A", "Class B", "Class C"};
+
+
+    public String[] getTitleStrings() {
+        return titleStrings;
+    }
+
+    public String[] getYearStrings() {
+        return yearStrings;
+    }
+
+    public String[] getMajorStrings() {
+        return majorStrings;
+    }
+
+    public String[] getClassStrings() {
+        return classStrings;
+    }
+}   //Main Class
--- app/src/main/java/raviwan/trongtum/ponpitoon/msccheck/RegisterActivity.java
+++ app/src/main/java/raviwan/trongtum/ponpitoon/msccheck/RegisterActivity.java
@@ -2,12 +2,104 @@ package raviwan.trongtum.ponpitoon.msccheck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private ImageView backImageView, humanImageView;
+    private EditText studentIDEditText, nameEditText, userEditText,
+            passwordEditText, addressEditText, phoneEditText, eMailEditText;
+    private Spinner titleSpinner, yearSpinner, majorSpinner, classSpinner;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Initial View
+        initialView();
+
+        //Create Spinner
+        createSpinner();
+
+        //Back Controller
+        backController();
+
+        //Save Controller
+        saveController();
+
+
+    }   // Main Method
+
+    private void createSpinner() {
+        MyConstant myConstant = new MyConstant();
+
+        //for Title
+        String[] titleStrings = myConstant.getTitleStrings();
+        ArrayAdapter<String> titleStringArrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, titleStrings);
+        titleSpinner.setAdapter(titleStringArrayAdapter);
+
+        // for Year
+        String[] yearStrings = myConstant.getYearStrings();
+        ArrayAdapter<String> yearStringArrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, yearStrings);
+        yearSpinner.setAdapter(yearStringArrayAdapter);
+
+        // for major
+        String[] majorStrings = myConstant.getMajorStrings();
+        ArrayAdapter<String> majorStringArrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, majorStrings);
+        majorSpinner.setAdapter(majorStringArrayAdapter);
+
+        // for Class
+        String[] classStrings = myConstant.getClassStrings();
+        ArrayAdapter<String> ClassStringArrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, classStrings);
+        classSpinner.setAdapter(ClassStringArrayAdapter);
+
+    }   // CreateSpinner
+
+    private void saveController() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }   // onClick
+        });
     }
-}
+
+    private void backController() {
+        backImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void initialView() {
+        backImageView = (ImageView) findViewById(R.id.imvBack);
+        humanImageView = (ImageView) findViewById(R.id.imvHuman);
+        studentIDEditText = (EditText) findViewById(R.id.edtStudentID);
+        nameEditText = (EditText) findViewById(R.id.edtName);
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        addressEditText = (EditText) findViewById(R.id.edtAddress);
+        phoneEditText = (EditText) findViewById(R.id.edtPhone);
+        eMailEditText = (EditText) findViewById(R.id.edtEmail);
+        titleSpinner = (Spinner) findViewById(R.id.spnTitle);
+        yearSpinner = (Spinner) findViewById(R.id.spnYear);
+        majorSpinner = (Spinner) findViewById(R.id.spnMajor);
+        classSpinner = (Spinner) findViewById(R.id.spnClass);
+        button = (Button) findViewById(R.id.btnSave);
+    }
+
+}   //Main Class
