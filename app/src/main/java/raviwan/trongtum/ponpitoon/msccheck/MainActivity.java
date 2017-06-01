5531cc51d68f8015ab84ab2a9c9eed1ad238ae32
Trongtum <bawnoi2538@gmail.com>
2017-06-01 13:41:11 +0700

    Design UI Reginter

--- app/src/main/java/raviwan/trongtum/ponpitoon/msccheck/MainActivity.java
+++ app/src/main/java/raviwan/trongtum/ponpitoon/msccheck/MainActivity.java
@@ -1,14 +1,53 @@
 package raviwan.trongtum.ponpitoon.msccheck;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Explicit ประกาศตัวเปล
+    private EditText userEditText,passwordEditText;
+    private TextView textView;
+    private Button button;
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initial View
+        initialVire();
+
+        //textView Controller
+        textViewController();
+
+    }   //เมททอสหลัก main mathod
+
+    private void textViewController() {
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }   //onClick
+
+
+
+        });
     }
-}
+
+    private void initialVire() {
+
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtNewRegister);
+        button = (Button) findViewById(R.id.btnLoing);
+    }
+}   //main Class นี่คือคลาสหลัก
